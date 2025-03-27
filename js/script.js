1ccfad0372a0d4b985a9b03e8dec1d691c950e40
andreamaluta <andreamaluta@gmail.com>
2025-03-27 15:50:33 +0100

    [UPD] made the form appear when the countdown finished

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const countdown = document.getElementById('countdown');
 const listNum = document.getElementById('numbers-list');
 const instruction = document.getElementById('instructions');
+const answer = document.getElementById('answers-form');
 
 let second = 5;
 let timeout = second*1000;
@@ -17,7 +18,7 @@ const clock = setInterval(function(){
         countdown.innerText = '0';
         instruction.innerText = "Inserisci tutti i numeri che ricordi (l'ordine non è importante)";
         listNum.classList.add('d-none');
-        
+        answer.classList.remove('d-none');
     }
 
     second--;
@@ -36,4 +37,6 @@ for(let i=0; i<numbers.length; i++){
     li.append(numbers[i]);
     listNum.appendChild(li);
 }
+
+
 
